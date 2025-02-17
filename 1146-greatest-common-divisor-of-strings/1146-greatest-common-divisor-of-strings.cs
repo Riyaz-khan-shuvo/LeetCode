195946e301424c4bb5e6dd081442a4ce3381b4a6
Riyaz Hossain <60660336+Riyaz-khan-shuvo@users.noreply.github.com>
2025-02-17 14:16:14 +0600

    Time: 0 ms (100%), Space: 41.5 MB (41.62%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public string GcdOfStrings(string str1, string str2) {
+                    if (!string.Equals(str1 + str2, str2 + str1))
+            {
+                return "";
+            }
+
+            int len1 = str1.Length;
+            int len2 = str2.Length;
+
+            while (len2 != 0)
+            {
+                int temp = len2;
+                len2 = len1 % len2;
+                len1 = temp;
+            }
+
+            return str1.Substring(0, len1);
+}
+}
